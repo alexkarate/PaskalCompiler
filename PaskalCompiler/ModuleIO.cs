e5545ce054f7d4d6fd71e654f3201013fe52f8f9
alexkarate <alexkarate.s@mail.ru>
2021-09-28 08:41:37 +0500

    Add IO Module class

---
+++ PaskalCompiler/ModuleIO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PaskalCompiler
+{
+    class ModuleIO
+    {
+        StreamReader file;
+        public ModuleIO(string filePath)
+        {
+            FileStream fs = File.OpenRead(filePath);
+            if(fs != null)
+            {
+                file = new StreamReader(fs);
+            }
+        }
+        ~ModuleIO()
+        {
+            if (file != null)
+            {
+                file.Close();
+                file = null;
+            }
+        }
+
+        CToken GetNextToken()
+        {
+            return new CToken();
+        }
+    }
+    enum TokenType { variable, something, el }
+    class CToken
+    {
+        TokenType _tt;
+        public override string ToString() { return "Generic token"; }
+    }
+}
