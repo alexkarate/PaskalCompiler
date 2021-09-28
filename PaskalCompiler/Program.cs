36057c929c2c53fd7a188320b127e49228ca19e7
alexkarate <alexkarate@DESKTOP-285NLG0>
2021-09-28 08:21:53 +0500

    Add project files.

---
+++ PaskalCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaskalCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
